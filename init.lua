22b55f22bca211a0ec9af89f2640605b5aa3b982
Andreas Högberg <andreas.hogberg@ctek.com>
2025-11-26 14:31:32 +0100

    Snacks

--- init.lua
+++ init.lua
@@ -205,6 +205,27 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      animate = { enabled = true },
+      git = { enabled = true },
+      bigfile = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
